ddddee7a6d4d8345d78806682fe48fd18b659e86
Zizou10 <paolo.pilati93@gmail.com>
2025-11-30 12:32:48 +0100

    02Strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,10 +2,28 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+let name = 'Paolo Pilati';
+console.log(name);
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+let text = 'Zizou ha indossato la maglia numero ';
+
+// Selezionare numero randoom
+let number = Math.floor(Math.random(0) * 100);
+
+//Stabilire i numeri che sono sempre true (Array = variabile con più valori)
+const numeriZidane = [5, 7, 10, 11, 21];
+
+// verificare se number è true, come fare? ---> includes() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+
+let bool = numeriZidane.includes(number);
+
+console.log(text + number);
+console.log("Check:", bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +32,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
